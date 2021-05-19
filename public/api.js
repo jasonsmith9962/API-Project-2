b34b7e76592109b9e92cab7ec1af44f07ed0de83
Jason smith <jasonsmith@Jasons-MacBook-Pro.local>
2021-05-18 20:55:54 -0400

    Final Draft

--- public/api.js
+++ public/api.js
@@ -1,13 +1,13 @@
 const key = 'T993Z7zFgfyLLCYhT9jWmpxiTTsHraX6';
-let searchBtn= document.querySelector("#search");
-let searchInput=document.getElementById("searchTerm");
+let searchBtn = document.querySelector("#search");
+let searchInput = document.getElementById("searchTerm");
 
 let div = document.querySelector(".results")
 
 function fetchResults() {
   console.log("fetchResults called", searchInput.value);
 
-  var searchValue=searchInput.value;
+  let searchValue = searchInput.value;
 
   const URL = `https://api.giphy.com/v1/gifs/search?api_key=${key}&limit=10&offset=0&rating=pg-13&lang=en&q=${searchValue}`;
   console.log(URL);
@@ -23,11 +23,13 @@ function fetchResults() {
 }
 searchBtn.addEventListener("click", fetchResults);
 function display(data){
+  while (div.firstChild) {
+    div.removeChild(div.firstChild); }
   console.log(data)
  data.forEach(item => {
    
-   let image= document.createElement("img")
-   image.src=item.images.original.url
+   let image = document.createElement("img")
+   image.src = item.images.original.url
    image.setAttribute("width","200px")
    div.appendChild(image)
   });
